7d7fe49465799184d517b9998c1c3753169644b8
guiborges77 <guironaldao@gmail.com>
2024-08-18 20:58:52 -0300

    update header

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -27,7 +27,7 @@ async function page() {
     <div className="h-full bg-background">
       <div className="border-b bg-card">
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-          <p className="text-3xl font-bold">Fala, {user.firstName} 🤘</p>
+          <p className="text-3xl">Fala, <span className="text-3xl font-bold">{user.firstName}</span> 🤘</p>
 
           <div className="flex items-center gap-3">
             <CreateTransactionDialog
